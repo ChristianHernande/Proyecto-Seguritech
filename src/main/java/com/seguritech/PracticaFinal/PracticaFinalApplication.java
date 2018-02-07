c0fca4737cd170e78bd446a130e28f404694ab10
unknown <christian.257@hotmail.com>
2018-02-07 13:48:04 -0600

    PrimerCommmit

---
+++ src/main/java/com/seguritech/PracticaFinal/PracticaFinalApplication.java
@@ -0,0 +1,12 @@
+package com.seguritech.PracticaFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaFinalApplication.class, args);
+	}
+}
